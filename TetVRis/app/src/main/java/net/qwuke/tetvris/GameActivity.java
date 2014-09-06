1df56e175de83f4ba55a711b4bcb7a4e01ebe2bc
Qwuke <tristan.e.mortimer@gmail.com>
2014-09-06 05:58:09 -0400

    Initial Project+Activities

---
+++ TetVRis/app/src/main/java/net/qwuke/tetvris/GameActivity.java
@@ -0,0 +1,46 @@
+package net.qwuke.tetvris;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.EyeTransform;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+/**
+ * Created by Tristan on 9/6/2014.
+ */
+public class GameActivity extends CardboardActivity implements CardboardView.StereoRenderer {
+
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    @Override
+    public void onDrawEye(EyeTransform eyeTransform) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i2) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
+}
---
+++ TetVRis/app/src/main/java/net/qwuke/tetvris/MainActivity.java
@@ -0,0 +1,7 @@
+package net.qwuke.tetvris;
+
+/**
+ * Created by Tristan on 9/6/2014.
+ */
+public class MainActivity {
+}
