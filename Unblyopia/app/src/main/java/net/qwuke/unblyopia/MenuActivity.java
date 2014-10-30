71bc9bce0312113fb26aa0b2eddb08aa2840c257
Qwuke <tristan.e.mortimer@gmail.com>
2014-10-30 07:06:41 -0400

    Basic main menu, material design tool bar
    Need to fix title, work on buttons themselves

--- Unblyopia/app/src/main/java/net/qwuke/unblyopia/MenuActivity.java
+++ Unblyopia/app/src/main/java/net/qwuke/unblyopia/MenuActivity.java
@@ -1,17 +1,32 @@
 package net.qwuke.unblyopia;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
-
-public class MenuActivity extends Activity {
+public class MenuActivity extends ActionBarActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+        final Button button = (Button) findViewById(R.id.button_tetris);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent("android.intent.action.GAME"));
+            }
+        });
+
     }
 
 
