d1877eaf2548d19a495e7297c1bff5f67b7c3f3f
Qwuke <tristan.e.mortimer@gmail.com>
2014-10-30 07:45:24 -0400

    Bold title workaround
    Yay

--- Unblyopia/app/src/main/java/net/qwuke/unblyopia/MenuActivity.java
+++ Unblyopia/app/src/main/java/net/qwuke/unblyopia/MenuActivity.java
@@ -2,8 +2,10 @@ package net.qwuke.unblyopia;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
@@ -18,7 +20,10 @@ public class MenuActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.app_name);
+        Resources res = getResources();
+        String text = String.format(res.getString(R.string.title_name));
+        CharSequence styledText = Html.fromHtml(text);
+        toolbar.setTitle(styledText);
         setSupportActionBar(toolbar);
         final Button button = (Button) findViewById(R.id.button_tetris);
         button.setOnClickListener(new View.OnClickListener() {
